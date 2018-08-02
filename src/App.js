2d435cc61360bad9ab47b5addf21fa2e686aca93
Oli Evans <oli@tableflip.io>
2018-08-02 12:56:01 +0100

    feat: prompt user to reload when updates available
    When the service worker finds updates, tell the user
    and ask them to reload.
    
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import { connect } from 'redux-bundler-react'
 import navHelper from 'internal-nav-helper'
 import IpldExploreForm from './explore/IpldExploreForm'
+import registerServiceWorker from './registerServiceWorker'
+import ipfsLogo from './navigation/ipfs-logo.svg'
 
 export class App extends Component {
   static propTypes = {
@@ -15,11 +17,22 @@ export class App extends Component {
     ]).isRequired
   }
 
-  componentWillMount () {
+  constructor (props) {
+    super(props)
+    this.state = {
+      showUpdateAvailable: true
+    }
+    registerServiceWorker({
+      onUpdate: () => this.setState({showUpdateAvailable: true})
+    })
+  }
+
+  componentDidMount () {
     this.props.doInitIpfs()
   }
 
   render () {
+    const {showUpdateAvailable} = this.state
     const Page = this.props.route
     const {embed} = this.props.queryObject
     return (
@@ -28,7 +41,7 @@ export class App extends Component {
           <header className='flex-ns items-center pa3 bg-navy bb bw3 border-aqua'>
             <div className='flex-auto'>
               <a href='#/' title='home' className='dib dib-ns'>
-                <img src='https://ipfs.io/images/ipfs-logo.svg' alt='IPFS' style={{height: 50, width: 117.5}} />
+                <img src={ipfsLogo} alt='IPFS' style={{height: 50, width: 117.5}} />
               </a>
               <h1 className='dib dn-l ma0 tr f3 fw2 montserrat aqua fr' style={{paddingTop: 14}}>IPLD EXPLORER</h1>
               <div className='dib ml3-ns pt2 pt0-ns'>
@@ -41,6 +54,13 @@ export class App extends Component {
         <div className='ph4-l pt4-l'>
           <Page embed={embed} />
         </div>
+        {showUpdateAvailable ? (
+          <div className='fixed bottom-0 w-100 tc'>
+            <div className='dib f5 lh-copy avenir ph4 pv3 white bg-green br2 br--top'>
+              A new version of IPLD Explorer is available, <a className='pointer underline link fw5' onClick={() => window.location.reload()}>please reload</a>
+            </div>
+          </div>
+        ) : null }
       </div>
     )
   }
