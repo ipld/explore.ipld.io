2a3b30201b78119405b55c2b04d9f0263c8e606d
Oli Evans <oli@tableflip.io>
2018-09-11 18:21:22 +0100

    chore: add missing i18n for UpdateAvailable
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/components/update/UpdateAvailable.js
+++ src/components/update/UpdateAvailable.js
@@ -5,7 +5,7 @@ const UpdateAvailable = () => {
   return (
     <div className='fixed bottom-0 w-100 tc'>
       <div className='dib f5 lh-copy avenir ph4 pv3 white bg-green br2 br--top'>
-        <Trans i18nkey='UpdateAvailable.paragraph1'>
+        <Trans i18nKey='UpdateAvailable.paragraph1'>
           A new version of IPLD Explorer is available, <a className='pointer underline link fw5' onClick={() => window.location.reload()}>please reload</a>
         </Trans>
       </div>
