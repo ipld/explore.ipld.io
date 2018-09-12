c0095b381e990fb26bbb679b78f1038bae2d2ae0
Oli Evans <oli@tableflip.io>
2018-09-12 11:21:58 +0100

    chore: update locales. Add sv locale.
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- src/i18n.js
+++ src/i18n.js
@@ -6,10 +6,11 @@ import LanguageDetector from 'i18next-browser-languagedetector'
 import en from 'i18next-icu/locale-data/en'
 import pl from 'i18next-icu/locale-data/pl'
 import pt from 'i18next-icu/locale-data/pt'
+import sv from 'i18next-icu/locale-data/sv'
 
 i18n
   .use(new ICU({
-    localeData: [en, pl, pt]
+    localeData: [en, pl, pt, sv]
   }))
   .use(XHR)
   .use(LanguageDetector)
