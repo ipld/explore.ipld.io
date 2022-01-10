2746626cbaea5b3a58dd3a8ad5428100a07ecf7b
dvd <d.dryjanski@gmail.com>
2022-01-10 04:02:38 -0800

    chore: bump ipld-explorer-components to 2.0.4 (#73)
    * bump ipld-explorer-components to 2.0.3
    * update to latest i18n pattern
    * fix: use latest js-ipfs and ipfs-http-client
    
    It was impossible to build old versions anymore. This switches to the
    latest js-ipfs. It also switched to ipld-explorer-components@2.0.4 which
    works around breaking changes in the Core JS API.
    
    In short, we now can use the `ipfs.block.get` from the latest js-ipfs
    without the need for rewriting entire IPLD Explorer.
    
    Co-authored-by: Marcin Rataj <lidel@lidel.org>

--- src/i18n.js
+++ src/i18n.js
@@ -1,34 +1,38 @@
 import i18n from 'i18next'
 import ICU from 'i18next-icu'
-import StaticHttpBackend from 'i18next-http-backend'
+import Backend from 'i18next-chained-backend'
+import LocalStorageBackend from 'i18next-localstorage-backend'
+import HttpBackend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
+import { version } from '../package.json'
 
-import ca from 'i18next-icu/locale-data/ca'
-import cs from 'i18next-icu/locale-data/cs'
-import da from 'i18next-icu/locale-data/da'
-import de from 'i18next-icu/locale-data/de'
-import en from 'i18next-icu/locale-data/en'
-import es from 'i18next-icu/locale-data/es'
-import fr from 'i18next-icu/locale-data/fr'
-import it from 'i18next-icu/locale-data/it'
-import ja from 'i18next-icu/locale-data/ja'
-import ko from 'i18next-icu/locale-data/ko'
-import nl from 'i18next-icu/locale-data/nl'
-import no from 'i18next-icu/locale-data/no'
-import pl from 'i18next-icu/locale-data/pl'
-import pt from 'i18next-icu/locale-data/pt'
-import ru from 'i18next-icu/locale-data/ru'
-import sv from 'i18next-icu/locale-data/sv'
-import zh from 'i18next-icu/locale-data/zh'
-
-const localeData = [ca, cs, da, de, en, es, fr, it, ja, ko, nl, no, pl, pt, ru, sv, zh]
+import locales from './lib/languages.json'
+export const localesList = Object.values(locales)
 
 i18n
-  .use(new ICU({ localeData }))
-  .use(StaticHttpBackend)
+  .use(ICU)
+  .use(Backend)
   .use(LanguageDetector)
   .init({
-    ns: ['explore'],
+    backend: {
+      backends: [
+        LocalStorageBackend,
+        HttpBackend
+      ],
+      backendOptions: [
+        { // LocalStorageBackend
+          defaultVersion: version,
+          expirationTime: (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 1 : 7 * 24 * 60 * 60 * 1000
+        },
+        { // HttpBackend
+          // ensure a relative path is used to look up the locales, so it works when loaded from /ipfs/<cid>
+          loadPath: 'locales/{{lng}}/{{ns}}.json'
+        }
+      ]
+    },
+    ns: ['app', 'welcome', 'status', 'files', 'explore', 'peers', 'settings', 'notify'],
+    defaultNS: 'app',
+    fallbackNS: 'app',
     fallbackLng: {
       'zh-Hans': ['zh-CN', 'en'],
       'zh-Hant': ['zh-TW', 'en'],
@@ -36,17 +40,13 @@ i18n
       default: ['en']
     },
     debug: process.env.DEBUG,
-    backend: {
-      // ensure a realtive path is used to look up the locales, so it works when used from /ipfs/<cid>
-      loadPath: 'locales/{{lng}}/{{ns}}.json'
-    },
     // react i18next special options (optional)
     react: {
       wait: true,
+      useSuspense: false,
       bindI18n: 'languageChanged loaded',
       bindStore: 'added removed',
-      nsMode: 'default',
-      useSuspense: false
+      nsMode: 'default'
     }
   })
 
