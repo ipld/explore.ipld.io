de2ec2fe25522f5a85e800989bf48580a998f298
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-04-03 17:38:14 -0700

    feat: explorer components update (#138)
    * deps: update ipld-explorer-components
    
    * chore: remove ignite-metrics

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ export class App extends Component {
     super(props)
     this.state = {
       showUpdateAvailable: false,
-      telemetry: null
     }
     if (props.registerServiceWorker) {
       props.registerServiceWorker({
@@ -27,25 +26,7 @@ export class App extends Component {
     }
   }
 
-  async initTelemetry() {
-    // embed checks if explorer is running stand alone or embeded in webui
-    const { embed } = this.props.queryObject
-    const { telemetry } = this.state
-
-    if (telemetry == null && !embed) {
-      const { BrowserMetricsProvider } = await import('@ipfs-shipyard/ignite-metrics/browser-vanilla')
-
-      const newTelemetry = new BrowserMetricsProvider({ appKey: '8bf7ee9fa77ec75c8173aa9fdc4877f5e2b61628' })
-      window.telemetry = newTelemetry
-      window.removeMetricsConsent = () => newTelemetry.removeConsent(['minimal'])
-      window.addMetricsConsent = () => newTelemetry.addConsent(['minimal'])
-
-      this.setState({ telemetry: newTelemetry })
-    }
-  }
-
   componentDidMount() {
-    this.initTelemetry()
     this.props.doInitHelia()
   }
 
