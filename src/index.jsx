8a48eaf38f43012a5a0ea70e8d9ae608162904c4
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-10-10 21:11:06 -0500

    chore: remove unused reduxStoreProvider

--- src/index.jsx
+++ src/index.jsx
@@ -23,11 +23,9 @@ console.log(`IPLD Explorer - v${appVersion} - https://github.com/ipfs-shipyard/i
 ReactDOM.render(
   <HeliaProvider>
     <ExploreProvider>
-      {/* <ReduxStoreProvider store={getStore()}> */}
         <I18nextProvider i18n={i18n}>
           <App registerServiceWorker={registerServiceWorker} />
         </I18nextProvider>
-      {/* </ReduxStoreProvider> */}
     </ExploreProvider>
   </HeliaProvider>,
   document.getElementById('root')
